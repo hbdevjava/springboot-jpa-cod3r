8c854a1c555282f6131bd7257441dc49136d9128
Hebert Brito <hbdev1211@gmail.com>
2023-07-13 20:06:15 -0300

    Primeiro Controller

--- src/main/java/com/estudos/springboot/exerciciosspringboot/controllers/PrimeiroController.java
+++ src/main/java/com/estudos/springboot/exerciciosspringboot/controllers/PrimeiroController.java
@@ -5,8 +5,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeiroController {
+
 	@GetMapping
+	private String opa() {
+		return "HB";
+	}
+
+	@GetMapping(path = "/1")
 	private String ola() {
+		return "Hebert";
+	}
+
+	@GetMapping(path = "/2")
+	private String nomeCompleto() {
 		return "Hebert Brito";
+	}
+
+	@GetMapping(path = { "/3", "/4", "/5" })
+	private String loveName() {
+		return "Vikas Brito";
 	}
 }
