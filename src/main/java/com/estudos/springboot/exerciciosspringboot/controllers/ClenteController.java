ec9f3905fe442a95cbf4e84ea2e6b185c728eeca
Hebert Brito <hbdev1211@gmail.com>
2023-07-13 22:35:37 -0300

    Passando Parâmetros para Web Service (@RequestParam) - PART 2

--- src/main/java/com/estudos/springboot/exerciciosspringboot/controllers/ClenteController.java
+++ src/main/java/com/estudos/springboot/exerciciosspringboot/controllers/ClenteController.java
@@ -3,16 +3,23 @@ package com.estudos.springboot.exerciciosspringboot.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudos.springboot.exerciciosspringboot.models.Cliente;
 
 /**
- * Claro! @PathVariable é uma anotação do Spring Framework que é utilizada para
- * mapear variáveis de modelo em URI de solicitação e usá-las como parâmetros de
- * método. É utilizado quando o valor da variável é passada diretamente na URL.
- * Já a anotação @RequestParam serve para pegar um trecho da url que geralmente
- * é dinâmico.
+ * @PathVariable é uma anotação do Spring Framework que é utilizada para mapear
+ *               variáveis de modelo em URI de solicitação e usá-las como
+ *               parâmetros de método. É utilizado quando o valor da variável é
+ *               passada diretamente na URL. Já a anotação @RequestParam serve
+ *               para pegar um trecho da url que geralmente é dinâmico.
+ * 
+ * @RequestParam é uma anotação do Spring Framework que é utilizada para extrair
+ *               parâmetros de consulta da URL. Por exemplo, se você tem uma URL
+ *               como http://localhost:8080/topicos?curso=Java, você pode
+ *               usar @RequestParam para extrair o valor do parâmetro “curso” da
+ *               URL
  */
 
 @RestController()
@@ -34,8 +41,14 @@ public class ClenteController {
 //		return new Cliente(3, "Dora Brito", "null");
 //	}
 
-	@GetMapping("/{id}")
-	public Cliente obterClientePorId(@PathVariable Integer id) {
-		return new Cliente(id, "Hebert Brito", "897.456.123-00");
+//	@GetMapping("/{id}")
+//	public Cliente obterClientePorId(@PathVariable Integer id) {
+//		return new Cliente(id, "Hebert Brito", "897.456.123-00");
+//	}
+
+	@GetMapping()
+	public Cliente obterClientePorId2(@RequestParam(name = "id") Integer id) {// ->
+																				// http://localhost:8080/clientes?id=123
+		return new Cliente(id, "Vikas Brito", "222.333.444.55");
 	}
 }
