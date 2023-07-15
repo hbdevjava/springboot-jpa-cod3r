67f71b747fb50132b111174373927ae2a3839c7d
Hebert Brito <hbdev1211@gmail.com>
2023-07-15 10:25:34 -0300

    Alterando o Produto #02

--- src/main/java/com/estudos/springboot/exerciciosspringboot/controller/ProdutoController.java
+++ src/main/java/com/estudos/springboot/exerciciosspringboot/controller/ProdutoController.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -46,11 +48,19 @@ public class ProdutoController {
 //	}
 
 //  SIMPLIFICANDO O CODIGO ACIMA
-	@PostMapping
+//	@PostMapping
+//	@ResponseBody
+//	public Produto novoProduto(@Valid Produto produto) {// -> @Valid valida as regras definidas pela anotation
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+
+	@RequestMapping(method = { RequestMethod.POST, RequestMethod.PUT })
 	@ResponseBody
-	public Produto novoProduto(@Valid Produto produto) {// -> @Valid valida as regras definidas pela anotation
+	public Produto alterarProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
+
 	}
 
 	@GetMapping
@@ -64,12 +74,15 @@ public class ProdutoController {
 		return produtoRepository.findById(id);
 	}
 
-	@PutMapping
-	public Produto alterarProduto(@Valid Produto produto) {
-		produtoRepository.save(produto);
-		return produto;
-
-	}
+//	@PutMapping
+//	public Produto alterarProduto(@Valid Produto produto) {
+//		produtoRepository.save(produto);
+//		return produto;
+//
+//	}
+	
+	
+	
 	
 
 }
