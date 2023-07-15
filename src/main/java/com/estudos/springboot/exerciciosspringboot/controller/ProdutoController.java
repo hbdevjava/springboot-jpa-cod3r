0a82dd651eac92a531eba23fa805d058ff135608
Hebert Brito <hbdev1211@gmail.com>
2023-07-15 11:39:47 -0300

    Consulta de Produto por Nome

--- src/main/java/com/estudos/springboot/exerciciosspringboot/controller/ProdutoController.java
+++ src/main/java/com/estudos/springboot/exerciciosspringboot/controller/ProdutoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -60,13 +62,21 @@ public class ProdutoController {
 	public Produto alterarProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
-
 	}
-
+	
 	@GetMapping
+	@ResponseBody
 	public List<Produto> obterProdutos() {
 		return produtoRepository.findAll();
 	}
+	
+	@GetMapping("/nome/{parteNome}")
+	
+	public List<Produto> obterProdutoPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findByNomeContaining(parteNome);
+	}
+	
+	
 
 	@GetMapping("/{id}")
 	@ResponseBody
--- src/main/java/com/estudos/springboot/exerciciosspringboot/model/repositories/ProdutoRepository.java
+++ src/main/java/com/estudos/springboot/exerciciosspringboot/model/repositories/ProdutoRepository.java
@@ -1,6 +1,8 @@
 package com.estudos.springboot.exerciciosspringboot.model.repositories;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.stereotype.Repository;
@@ -9,5 +11,8 @@ import com.estudos.springboot.exerciciosspringboot.model.entities.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-//-> extends CrudRepository<Produto, Long> tb pode ser usada.
+//-> extends CrudRepository<Produto, Long> tb pode ser usada.(versão simples com operações basicas de CRUD)
+ 
+	public List<Produto> findByNomeContaining(String parteNome);
+//-> CASO O METODO NA EXISTA: ele pode ser implementado na interface e o springBoot se encarrega de repassar quando for chamado	
 }
